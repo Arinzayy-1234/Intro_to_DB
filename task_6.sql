be1358b4e37db2b7b83a191c85d135a5370809c7
Arinzayyy - Kris <103210483+Arinzayy-1234@users.noreply.github.com>
2025-12-14 22:39:53 +0100

    ALX checker -> task_6.sql doesn't contain: ["124 Happiness  Ave."]

--- task_6.sql
+++ task_6.sql
@@ -4,14 +4,14 @@
 USE alx_book_store;
 
 -- Inserts multiple rows of data into the CUSTOMERS table using a single statement.
--- NOTE: Using 'customer' (singular, lowercase) to satisfy the checker's specific string requirement.
+-- NOTE: The address for ID 2 now includes the required double space.
 INSERT INTO customer (
-    customer_id,   -- Required to be lowercase 'customer_id'
+    customer_id,
     customer_name,
     email,
     address
 )
 VALUES 
-    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
+    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'), -- <-- Corrected to double space
     (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
     (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
